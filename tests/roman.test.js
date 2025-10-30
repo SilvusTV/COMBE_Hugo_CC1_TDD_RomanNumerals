a7ead4b4263f1eb604008c242e7af239ffaa819b
Silvus <hugo.combe.84@gmail.com>
2025-10-30 16:12:15 +0100

    red2 : add two substraction tests

--- tests/roman.test.js
+++ tests/roman.test.js
@@ -19,4 +19,12 @@ describe('Roman numerals - Red 1', () => {
   test('should throw if input value is not less than 3999',()=>{
     expect(() => toRoman(4000)).toThrow('Input value must be less than 3999')
   })
+
+  //Red 2
+  test('should converts 4 to Roman numerals "IV"',() => {
+    expect(toRoman(4)).toBe('IV');
+  })
+  test('should converts 9 to Roman numerals "IX"',() => {
+    expect(toRoman(9)).toBe('IX');
+  })
 });
