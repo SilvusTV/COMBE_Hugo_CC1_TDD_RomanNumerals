6f21110faef0dbc961dad3a2e1b477428361964e
Silvus <hugo.combe.84@gmail.com>
2025-10-30 16:50:35 +0100

    red 3 : add every possible test to be sure of the value returned

--- tests/roman.test.js
+++ tests/roman.test.js
@@ -1,11 +1,12 @@
 // tests/roman.test.js
 const { toRoman } = require('../src/roman');
 
-describe('Roman numerals - Red 1', () => {
+describe('Roman numerals', () => {
+
+  //Red 1
   test('should converts 1 to Roman numerals "I"', () => {
     expect(toRoman(1)).toBe('I');
   });
-
   test('should converts 2 to Roman numerals "II"', () => {
     expect(toRoman(2)).toBe('II');
   });
@@ -26,5 +27,48 @@ describe('Roman numerals - Red 1', () => {
   })
   test('should converts 9 to Roman numerals "IX"',() => {
     expect(toRoman(9)).toBe('IX');
+  })
+
+  //Red3
+  test('should throw exception if input value is not an integer', () => {
+    expect(() => toRoman(3.5)).toThrow('Input value must be an integer')
+  })
+  test('should throw exception if input value in not a number', () => {
+    expect(() => toRoman("a")).toThrow('Input value must be an integer')
+  })
+  test('should every Roman letter I,V,X,L,C,D,M appears at least once', () => {
+    const seen = new Set();
+
+    for (let n = 1; n <= 3999; n++) {
+      const r = toRoman(n);
+      for (const ch of r) seen.add(ch);
+      if (seen.size === 7) break; // optimisation: on peut s’arrêter tôt
+    }
+
+    const expected = new Set(['I', 'V', 'X', 'L', 'C', 'D', 'M']);
+    expected.forEach(ch => expect(seen.has(ch)).toBe(true));
+  });
+  test('sould at least one number produces each subtractive pair', () => {
+    const targets = new Map([
+      ['IV', 4],
+      ['IX', 9],
+      ['XL', 40],
+      ['XC', 90],
+      ['CD', 400],
+      ['CM', 900],
+    ]);
+
+    for (const [pair, n] of targets.entries()) {
+      expect(toRoman(n)).toContain(pair);
+    }
+  });
+  test('should convert 58 to "LVIII"', () => {
+    expect(toRoman(58)).toBe('LVIII');
+  })
+  test('should convert 1994 to "MCMXCIV"', () => {
+    expect(toRoman(1994)).toBe('MCMXCIV');
+  })
+  test('should convert 3999 to "MMMCMXCIX"', () => {
+    expect(toRoman(3999)).toBe('MMMCMXCIX');
   })
 });
